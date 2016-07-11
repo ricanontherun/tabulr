cab34380f72b0e87f06eeecbaa11630b3e068034
ricanontherun@gmail.com <ricanontherun@gmail.com>
2016-07-10 21:44:13 -0400

    Initial push

---
+++ build/CMakeFiles/tabulr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tabulr.dir/main.cpp.o"
+  "tabulr.pdb"
+  "tabulr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tabulr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/row.h
@@ -0,0 +1,36 @@
+// Copyright (C) 2016 Christian Roman
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#ifndef TABULR_ROW_H
+#define TABULR_ROW_H
+
+#include <memory>
+#include <vector>
+
+#include <abstract_column.h>
+
+namespace Tabulr
+{
+
+class Row
+{
+    public:
+        Row() {};
+    private:
+        std::vector<std::unique_ptr<AbstractColumn>> columns;
+};
+
+}
+
+#endif
