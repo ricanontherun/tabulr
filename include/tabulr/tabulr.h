e4d685eec1142ad54e5792f19c1d632400d41981
Christian Roman <ricanontherun@gmail.com>
2016-07-18 16:36:25 +0000

    Added a cumulative include

---
+++ include/tabulr/tabulr.h
@@ -0,0 +1,9 @@
+#ifndef TABULR_CUMULATIVE_INCLUDE_H
+#define TABULR_CUMULATIVE_INCLUDE_H
+
+#include <tabulr/table.h>
+#include <tabulr/row.h>
+#include <tabulr/cell.h>
+#include <tabulr/format.h>
+
+#endif
--- main.cpp
+++ main.cpp
@@ -13,8 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <tabulr/table.h>
-#include <tabulr/format.h>
+#include <tabulr/tabulr.h>
 
 #include <vector>
 #include <iostream>
